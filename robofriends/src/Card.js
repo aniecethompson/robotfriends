6eeb7b210b88a7304259f812c641357b7c7079ec
aniecethompson <aniecethompson@gmail.com>
2020-03-17 23:49:03 -0400

    added card component

---
+++ robofriends/src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+        return (
+            <div>
+                <img src="https://robohash.org/test?200x200 " alt= "robots"/>
+                <h2>Jane Doe</h2>
+                <p>janedoe@gmail.com</p>
+            </div>
+        );
+}
+
+export default Card;
